676c43e26a5ec42511cf50c4573bc2b5d1d7ee4b
Augment Chinedu <46214862+augmentchinedu@users.noreply.github.com>
2026-02-09 10:22:36 +0000

    gql token bearer updated

--- gql/index.js
+++ gql/index.js
@@ -1,4 +1,5 @@
 import { GraphQLClient, gql } from "graphql-request";
+import Cookies from "js-cookie";
 
 const isCodespaces = window.location.hostname.includes("github.dev");
 
@@ -6,12 +7,12 @@ const endpoint = isCodespaces
   ? `https://${window.location.hostname.replace(/-\d+/, "-3000")}/graphql`
   : `https://${window.location.host}/graphql`;
 
-// HTTP client always reads latest token from localStorage
+// HTTP client always reads latest token from cookie
 export const client = new GraphQLClient(endpoint, {
   credentials: "include",
   headers: {
     get Authorization() {
-      const token = localStorage.getItem("token");
+      const token = Cookies.get("token"); // read from cookie
       return token ? `Bearer ${token}` : "";
     },
   },
